69c47099e9f9a357599438a879f9433f913105b8
Matheus Braga Ribeiro <matheus.ribeiro@fotosensores.com>
2024-10-11 16:26:29 -0300

    #FEAT: Limpando componente de produtos e adicionando CarrinhoContext e função de adicionar produto

--- src/components/Produtos/index.jsx
+++ src/components/Produtos/index.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useContext } from "react";
 import Produto from "./Produto";
 import produtos from "@/mocks/produtos.json";
 import Titulo from "@/components/Titulo";
+import { CarrinhoContext } from "../../context/CarrinhoContext";
 
-const Produtos = ({ adicionarProduto }) => {
+const Produtos = () => {
+  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+
+  const adicionarProduto = (novoProduto) => {
+    const temOProduto = carrinho.some(
+      (itemCarrinho) => itemCarrinho.id === novoProduto.id
+    );
+
+    if (!temOProduto) {
+      novoProduto.quantidade = 1;
+      return setCarrinho([...carrinho, novoProduto]);
+    }
+
+    return setCarrinho(
+      carrinho.map(
+        (itemCarrinho) => {
+          if (itemCarrinho.id === novoProduto.id) itemCarrinho.quantidade += 1;
+          return itemCarrinho;
+        }
+      )
+    );
+  };
+
   return (
     <section role="produtos" aria-label="Produtos que estão bombando!">
       <Titulo>Produtos que estão bombando!</Titulo>
